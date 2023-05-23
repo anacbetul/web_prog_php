bdafa76e52d5516eb259fb2308dc5ba46bb7d3f7
Betül Anaçoğlu <101509982+anacbetul@users.noreply.github.com>
2023-05-23 18:01:36 +0300

    Update linkler.php

--- linkler.php
+++ linkler.php
@@ -1,13 +1,16 @@
 <img src="resim.png" height="120" border="0" align="right" hspace="20" vspace="15" /></br>
-<p><font face="arial">Adres: </br >Yüzevler Mah. Mustafa Kemal Paşa Cad.
-Şekerbank Üstü K: 2 - AMASYA
-</br >
-</br >
-Tel: (0(358) 210 00 12)
-</br >
-</br >
-dr.turkan@amasyaortodontist.com
-</br >
-</br >
-</font></p>
-<a href="https://amasyaortodontist.com/" target="_blank">Resmi web sitesi</a> 
+<p>
+    <font face="arial">Adres: </br >Yüzevler Mah. Mustafa Kemal Paşa Cad.
+    Şekerbank Üstü K: 2 - AMASYA
+    </br >
+    </br >
+    Tel: (0(358) 210 00 12)
+    </br >
+    </br >
+    dr.turkan@amasyaortodontist.com
+    </br >
+    </br >
+    <a href="https://amasyaortodontist.com/" target="_blank">Resmi web sitesi</a> </br ></br >
+    <a href="https://github.com/anacbetul/web_prog_php" target="_blank">Github linkim</a></br >
+    </font>
+</p>
